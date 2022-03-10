63665609a51963d73ed0726854d690ad94e22d89
Chris Baum <Christopher.Baum@valpo.edu>
2022-03-09 22:02:12 -0500

    commit tree 5d8777b7c657d1ab668c51fba4fb9a8a4cbd1526

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+
+
+let returnFirstTwoDrivers = function(drivers){
+  return drivers.slice(0, 2)
+}
+
+let returnLastTwoDrivers = function(drivers){
+  return drivers.slice(-2)
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+let createFareMultiplier = function(multiplier){
+  return function(value){
+    return value * multiplier
+  }
+}
+
+let fareDoubler = createFareMultiplier(2)
+
+let fareTripler = createFareMultiplier(3)
+
+let selectDifferentDrivers = function(arrayOfDrivers, selectedFunction){
+  return selectedFunction(arrayOfDrivers)
+}
